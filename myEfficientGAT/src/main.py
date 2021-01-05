9ffa60c2fcfe390b9d4fceb100d72b4b9aae8303
unknown <fxcyf1222@163.com>
2021-01-06 01:28:31 +0800

    EfficientGAT-new

---
+++ myEfficientGAT/src/main.py
@@ -0,0 +1,51 @@
+import torch
+import argparse
+from clustering import ClusteringMachine
+from clustergat import ClusterGATTrainer
+from utils import tab_printer, graph_reader, feature_reader, target_reader
+
+
+def main():
+    """
+    Parsing command line parameters, reading data, graph decomposition, fitting a ClusterGCN and scoring the model.
+    """
+    # parameters setting
+    parser = argparse.ArgumentParser(description="Run .")
+    parser.add_argument("--edge-path", nargs="?", default="../input/edges.csv", help="Edge list csv.")
+    parser.add_argument("--features-path", nargs="?", default="../input/features.csv", help="Features json.")
+    parser.add_argument("--target-path", nargs="?", default="../input/target.csv", help="Target classes csv.")
+    parser.add_argument("--clustering-method", nargs="?", default="metis", help="Clustering method for graph "
+                                                                                "decomposition. Default is the metis "
+                                                                                "procedure.")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed for train-test split. Default is 42.")
+    parser.add_argument('--cuda', action='store_true', default=False, help='Use CUDA training.')  # 无需带参数
+    parser.add_argument("--epochs", type=int, default=200, help="Number of training epochs. Default is 200.")
+    parser.add_argument("--dropout", type=float, default=0.5, help="Dropout parameter. Default is 0.5.")
+    parser.add_argument("--learning-rate", type=float, default=0.01, help="Learning rate. Default is 0.01.")
+    parser.add_argument("--test-ratio", type=float, default=0.1, help="Test data ratio. Default is 0.1.")
+    parser.add_argument("--cluster-number", type=int, default=10, help="Number of clusters extracted. Default is 10.")
+
+    parser.add_argument('--hidden', type=int, default=8, help='Number of hidden units.')
+    parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
+    parser.add_argument('--patience', type=int, default=100, help='Patience')
+
+    args = parser.parse_args()  # args = parser.parse_args()
+
+    torch.manual_seed(args.seed)  # random seed
+    tab_printer(args)
+    # load data and metis
+    graph = graph_reader(args.edge_path)  # graph = nx.Graph(...edges)
+    features = feature_reader(args.features_path)  # features = coo_matrix(...)
+    target = target_reader(args.target_path)  # target = np.array(size=(-1,1))
+
+    clustering_machine = ClusteringMachine(args, graph, features, target)
+    clustering_machine.decompose()
+
+
+    myGAT = ClusterGATTrainer(args, clustering_machine)
+    myGAT.train()
+    myGAT.test()
+
+
+if __name__ == "__main__":
+    main()
